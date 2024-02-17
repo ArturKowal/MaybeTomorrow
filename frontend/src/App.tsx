e18ddece86cfa90c26f6ea9d74151a75efc77743
ArturKowal <artur.kowal@smcebi.edu.pl>
2024-02-17 21:21:40 +0100

    added yab name and logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@ import "/src/App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import PageSelector from "./pages/PageSelector";
+import Favicon from "react-favicon";
+import icon from "./components/images/logo_MB_MINI.png";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="container mainLayout">
+      <Favicon url={icon} />
       <Router>
         <div className="container navbarOwn">
           <Navbar />
